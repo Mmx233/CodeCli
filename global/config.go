d88be73e10bb7263179b44ad81ab7da33757e127
Mmx233 <admin@mmxblog.com>
2023-01-07 06:14:10 +0800

    fix: 配置文件加载路径变动问题

--- global/config.go
+++ global/config.go
@@ -2,6 +2,7 @@ package global
 
 import (
 	"github.com/Mmx233/CodeCli/global/models"
+	"github.com/Mmx233/CodeCli/pkg/file"
 	"github.com/Mmx233/config"
 	"github.com/Mmx233/tool"
 	"github.com/mitchellh/go-homedir"
@@ -12,24 +13,23 @@ import (
 var Config models.Config
 
 func init() {
+	home, e := homedir.Expand("~/.CodeCli.yaml")
+	if e != nil {
+		log.Fatalln(e)
+	}
 	c := config.NewConfig(&config.Options{
+		Path:   file.JoinPath(home, ".CodeCli.yaml"),
 		Config: &Config,
 		Default: &models.Config{
 			Default: models.Default{
 				GitSite: "github.com",
 			},
 			Storage: models.Storage{
-				ProjectDir: "~/project",
+				ProjectDir: file.JoinPath(home, "project"),
 			},
 		},
 	})
-	e := c.Load()
-	if e != nil {
-		log.Fatalln(e)
-	}
-
-	Config.Storage.ProjectDir, e = homedir.Expand(Config.Storage.ProjectDir)
-	if e != nil {
+	if e = c.Load(); e != nil {
 		log.Fatalln(e)
 	}
 

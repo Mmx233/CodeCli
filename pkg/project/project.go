60bfb6560fa89fc7e0a2defc96e9e84cdb6824b9
Mmx233 <admin@mmxblog.com>
2023-01-07 18:52:29 +0800

    improve: 拆分 project.Open 函数

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -10,34 +10,45 @@ import (
 	"strings"
 )
 
-func Open(addr string) error {
+// CompleteAddr 填充简写为完整 addr
+func CompleteAddr(addr string) (string, error) {
 	switch len(strings.Split(addr, "/")) {
 	case 1:
 		if global.Config.Default.Username == "" {
-			return util.ErrEmptyDefaultUsername
+			return "", util.ErrEmptyDefaultUsername
 		}
 		addr = global.Config.Default.Username + "/" + addr
 		fallthrough
 	case 2:
 		if global.Config.Default.GitSite == "" {
-			return util.ErrEmptyDefaultGitSite
+			return "", util.ErrEmptyDefaultGitSite
 		}
 		addr = global.Config.Default.GitSite + "/" + addr
 	case 3:
 		break
 	default:
-		return util.ErrUnknownInput
+		return "", util.ErrUnknownInput
 	}
+	return addr, nil
+}
 
+func ConvertAddrToPath(addr string) (dir string, path string) {
 	split := strings.Split(addr, "/")
-	projectDir := file.JoinPath(append([]string{global.Config.Storage.ProjectDir}, split[:2]...)...)
+	dir = file.JoinPath(append([]string{global.Config.Storage.ProjectDir}, split[:2]...)...)
+	path = file.JoinPath(dir, split[2])
+	return
+}
 
-	e := os.MkdirAll(projectDir, 0600)
+func Open(addr string) error {
+	addr, e := CompleteAddr(addr)
 	if e != nil {
 		return e
 	}
 
-	projectPath := file.JoinPath(projectDir, split[2])
+	projectDir, projectPath := ConvertAddrToPath(addr)
+	if e = os.MkdirAll(projectDir, 0600); e != nil {
+		return e
+	}
 
 	if !tool.File.Exists(projectPath) {
 		if e = cmd.Clone("https://"+addr+".git", projectPath); e != nil {

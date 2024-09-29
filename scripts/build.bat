2f8e1394f6f15ad37aca055ecbf3324e8bca1319
Mmx <green.dusk8264@fastmail.com>
2024-09-29 21:09:50 +0800

    build: add release build for linux

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,3 @@
 @echo off
 SET /p v=Version:
-release ./cmd/code --os windows --arch amd64,386,arm64 --ldflags="-X main.Version=%v%" -c tar.gz
+release ./cmd/code --os windows,linux --extra-arches --ldflags="-X main.Version=%v%" -c tar.gz

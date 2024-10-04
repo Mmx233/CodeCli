b29e95d5330cf0aa2232fb4eabb09e66a9ae1b39
Mmx <green.dusk8264@fastmail.com>
2024-10-04 17:32:33 +0800

    feat: use go exec to implement windows create process

--- pkg/idea/idea_windows.go
+++ pkg/idea/idea_windows.go
@@ -2,7 +2,7 @@ package idea
 
 import (
 	"golang.org/x/sys/windows"
-	"strings"
+	"os/exec"
 	"syscall"
 )
 
@@ -17,52 +17,26 @@ type windowsExec struct {
 	dir string
 }
 
-func (a windowsExec) program(name string, args ...string) (cmdLine *uint16, workdir *uint16, err error) {
-	program := name
-	if len(args) != 0 {
-		program += " " + strings.Join(args, " ")
-	}
-	program = strings.Replace(program, `/`, `\\`, -1)
-	cmdLine, err = windows.UTF16PtrFromString(program)
-	if err != nil {
-		return
-	}
-
-	if a.dir != "" {
-		workdir = windows.StringToUTF16Ptr(strings.Replace(a.dir, `/`, `\\`, -1))
-	}
-	return
-}
-
 func (a windowsExec) Command(name string, args ...string) error {
-	program, workdir, err := a.program(name, args...)
-	if err != nil {
-		return err
+	cmd := exec.Command(name, args...)
+	cmd.Dir = a.dir
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		HideWindow:       false,
+		CreationFlags:    windows.CREATE_NEW_CONSOLE,
+		NoInheritHandles: true,
 	}
-
-	var procInfo syscall.ProcessInformation
-	startupInfo := &syscall.StartupInfo{
-		Flags:      windows.STARTF_USESHOWWINDOW,
-		ShowWindow: windows.SW_NORMAL,
-	}
-	return syscall.CreateProcess(
-		nil, program,
-		nil, nil, false, windows.CREATE_NEW_CONSOLE, nil,
-		workdir, startupInfo, &procInfo)
+	return cmd.Start()
 }
 
 func (a windowsExec) CreateProcess(name string, args ...string) error {
-	program, workdir, err := a.program(name, args...)
-	if err != nil {
-		return err
+	cmd := exec.Command(name, args...)
+	cmd.Dir = a.dir
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		HideWindow:       true,
+		CreationFlags:    windows.CREATE_NEW_CONSOLE | windows.CREATE_NO_WINDOW,
+		NoInheritHandles: true,
 	}
-
-	var procInfo syscall.ProcessInformation
-	startupInfo := &syscall.StartupInfo{}
-	return syscall.CreateProcess(
-		nil, program,
-		nil, nil, false, windows.CREATE_NO_WINDOW, nil,
-		workdir, startupInfo, &procInfo)
+	return cmd.Start()
 }
 func (a windowsExec) SetDir(dir string) ExecInterface {
 	return windowsExec{dir: dir}
